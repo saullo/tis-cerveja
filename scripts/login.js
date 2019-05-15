7a2fd731d96fa5430b5b44e1de68cb6047e5d88c
Saullo Bretas Silva <saullo.silva303@gmail.com>
2019-05-15 15:31:22 -0300

    js login, register

---
+++ scripts/login.js
@@ -0,0 +1,72 @@
+$(document).ready(function() {
+  init()
+})
+
+var name, password, email, birth, checkbox
+
+function init() {
+  initVariables()
+  initListeners()
+}
+
+function initVariables() {
+  // Set all the variables
+  let form = $('#login')
+  let formContext = form.children()
+
+  email = formContext.children("#email")
+  password = formContext.children("#password")
+}
+
+function initListeners() {
+  $('#submit').click(function(e) {
+    e.preventDefault()
+    validate()
+  })
+}
+
+function validate() {
+  if(!checkEmail() && !checkPassword() ) {
+    login()
+  }
+}
+
+function login() {
+  const db = JSON.parse(localStorage.getItem('users'))
+  console.log(db)
+
+  if (db.email != email[0].value) {
+    let error = $('#error-email')
+    error.html('O email não existe')
+  } else if(db.password != password[0].value) {
+    let error = $('#error-password')
+    error.html('A senha está errada')
+  } else {
+    localStorage.setItem('isLogged', true)
+    window.location.replace("produtos.html");
+  }
+}
+
+function checkPassword() {
+  let error = $('#error-password')
+  if (password[0].value == '') {
+    error.html('A senha é obrigatoria')
+    return true;
+  } else if(password[0].value.length < 6) {
+    error.html('A senha deve ter no minimo 6 digitos')
+    return true;
+  } else {
+    error.html('')
+    return false;
+  }
+}
+function checkEmail() {
+  let error = $('#error-email')
+  if (email[0].value == '') {
+    error.html('O email é obrigatorio')
+    return true;
+  } else {
+    error.html('')
+    return false;
+  }
+}
--- scripts/register.js
+++ scripts/register.js
@@ -2,29 +2,105 @@ $(document).ready(function() {
   init()
 })
 
+var name, password, email, birth, checkbox
+
 function init() {
-  register()
+  initVariables()
+  initListeners()
+}
+
+function initVariables() {
+  // Set all the variables
+  let form = $('#register')
+  let formContext = form.children()
+  let formContextRow = formContext.children()
+
+  name = formContextRow.children("#name")
+  console.log(name)
+  password = formContextRow.children("#password")
+  checkbox = formContextRow.children("#checkbox")
+  email = formContext.children("#email")
+  birth = formContext.children("#birth")
+}
+
+function initListeners() {
+  $('#submit').click(function(e) {
+    e.preventDefault()
+    validate()
+  })
+}
+
+function validate() {
+  if(!checkName() && !checkPassword() && !checkBirth() && !checkEmail() && !checkCheckbox()) {
+    register()
+  }
 }
 
 function register() {
-  $('#submit').click(function(e) {
-    e.preventDefault();
+  let user = {
+    name: name[0].value,
+    email: email[0].value,
+    password: password[0].value,
+    birth: birth[0].value
+  }
+  localStorage.setItem('users', JSON.stringify(user))
+  window.location.replace("login.html");
+}
 
-    let form = $('#register')
-    let formContext = form.children()
+function checkName() {
+  let error = $('#error-name')
+  console.log(name)
+  if (name[0].value == '') {
+    error.html('Nome é obrigatorio')
+    return true;
+  } else {
+    error.html('')
+    return false;
+  }
+}
 
-    let name = formContext.children().children('#name').val()
-    let password = formContext.children().children("#password").val()
-    let email = formContext.children("#email").val()
-    let birth = formContext.children("#birth").val()
+function checkPassword() {
+  let error = $('#error-password')
+  if (password[0].value == '') {
+    error.html('A senha é obrigatoria')
+    return true;
+  } else if(password[0].value.length < 6) {
+    error.html('A senha deve ter no minimo 6 digitos')
+    return true;
+  } else {
+    error.html('')
+    return false;
+  }
+}
 
-    let user = {
-      name: name,
-      email: email,
-      password: password,
-      birth: birth
-    }
-    localStorage.setItem('users', JSON.stringify(user))
+function checkBirth() {
+  let error = $('#error-birth')
+  if (birth[0].value == '') {
+    error.html('A data de nascimento é obrigatoria')
+    return true;
+  } else {
+    error.html('')
+    return false;
+  }
+}
 
-  })
+function checkEmail() {
+  let error = $('#error-email')
+  if (email[0].value == '') {
+    error.html('O email é obrigatorio')
+    return true;
+  } else {
+    error.html('')
+    return false;
+  }
+}
+
+function checkCheckbox() {
+  let error = $('error-checkbox') 
+  if (checkbox[0].checked == false) {
+    error.html('Você precisa aceitar os termos')
+    return true
+  } else {
+    return false
+  }
 }
